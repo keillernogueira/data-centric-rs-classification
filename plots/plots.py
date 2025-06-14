9bca8591e4d54bea9c7e824f003b8090fff2ef68
Keiller Nogueira <keillernogueira@gmail.com>
2025-06-14 12:18:47 +0100

    Plots

--- plots/plots.py
+++ plots/plots.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import argparse
 import warnings
 import pickle
 from collections import defaultdict
@@ -11,9 +12,13 @@ import scipy.stats
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from numpy import finfo
 from scipy.interpolate import make_interp_spline, BSpline
 from matplotlib.patches import Rectangle
+import matplotlib.patheffects as pe
 from rasterio.enums import Resampling
+from scipy.special import softmax
+from scipy.stats import entropy
 
 
 # ignore NotGeoreferencedWarning
@@ -439,13 +444,9 @@ def read_label_patch(label_path, image_name, coord_x, coord_y, dfc, patch_size=2
     if dfc is False:
         label_path = os.path.join(label_path, image_name + '.tif')
     else:  # dfc
-        if vec[0] == 'Nice':
-            region = vec[0]
-            img_name = vec[1]
-        else:
-            region = '_'.join(vec[:2])
-            img_name = vec[2]
-        label_path = os.path.join(label_path, region, dataset_subpath[dataset][2], img_name + '_UA2012.png')
+        region, image = image_name.split("*")
+
+        label_path = os.path.join(label_path, region, 'UrbanAtlas', image + '_UA2012.tif')
 
     mask = _load_target(label_path)
     mask_patch = mask[:, coord_x:coord_x + patch_size, coord_y:coord_y + patch_size]
@@ -482,15 +483,15 @@ def get_class_frequency(rank_path, label_path, dfc=False):
 
             key = f"{row[0]}_{row[3]}_{row[4]}"
         else:
-            patch = read_label_patch(label_path, row[0] + "_" + row[1], row[2], row[3], dfc, patch_size=256)
-            class_freq = np.bincount(patch.flatten(), minlength=14)
+            patch = read_label_patch(label_path, row[0] + "*" + row[1], row[2], row[3], dfc, patch_size=256)
+            class_freq = np.bincount(patch.flatten(), minlength=16)
 
             key = f"{row[0]}_{row[1]}_{row[2]}_{row[3]}"
         key_vals[key] = class_freq.tolist()
     return key_vals
 
 
-def plot_acc_freq(keys, class_freq, title):
+def plot_bar_acc_freq(keys, class_freq, title):
     def non_decreasing(L):
         return all(x <= y for x, y in zip(L, L[1:]))
 
@@ -535,20 +536,122 @@ def plot_acc_freq(keys, class_freq, title):
     plt.savefig(title + "_average_rank_class_freq.png", dpi=4000)
 
 
-if __name__ == "__main__":
-    operation = 'acc_class_frequency'
+def plot_line_acc_freq(keys, class_freq, title):
+    plt.figure(figsize=(10, 6), layout='compressed')
 
-    if operation == 'encoded_labels':
+    vaihingen_potsdam_color = [(1, 1, 1), (0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0, 0)]
+    vaihingen_potsdam_classes = ['Street', 'Building', 'Grass', 'Tree', 'Car', 'Surfaces']
+    dfc2022_color = ["#DB5F57", "#DB9757", "#DBD057", "#ADDB57", "#75DB57", "#7BC47B",
+                     "#58B158", "#D4F6D4", "#B0E2B0", "#008000", "#58B0A7", "#995D13", "#579BDB", "#0062FF", "#231F20"]
+    dfc2022_classes = ['Fabric', 'Industrial', 'Mine', 'Vegetation', 'Arable land', 'Crops', 'Pastures',
+                       'Cultivation', 'Orchards', 'Forests', 'Herbaceous',
+                       'No vegetation', 'Wetlands', 'Water', 'Clouds']
+
+    color = None
+    classes = None
+    if title == 'Vaihingen' or title == 'Potsdam':
+        color = vaihingen_potsdam_color
+        classes = vaihingen_potsdam_classes
+    else:
+        color = dfc2022_color
+        classes = dfc2022_classes
+
+    xs = np.linspace(1, len(keys), len(keys))
+    y = []
+    for i, k in enumerate(keys):
+        if not y:
+            y.append(np.asarray(class_freq[k]))
+        else:
+            y.append(y[-1] + np.asarray(class_freq[k]))
+    y_np = np.asarray(y).T
+    if title == 'DFC2022':
+        y_np = y_np[1:, :]
+    print('comparing x, y', len(keys), np.asarray(y).shape, np.asarray(y_np).shape)
+
+    for i, yc in enumerate(y_np):
+        # print(yc.shape)
+        if title != "DFC2022" and i == 0:
+            plt.plot(xs, yc, color=color[i], linewidth=1, label=classes[i],
+                     path_effects=[pe.Stroke(linewidth=1.5, foreground='black'), pe.Normal()])
+        else:
+            plt.plot(xs, yc, color=color[i], linewidth=1, label=classes[i])
+
+    # plt.rcParams.update({'font.size': 22})
+    plt.xlabel("Patch sorted by average rank", fontsize=22)
+    plt.ylabel("Class Frequency", fontsize=22)
+    plt.xticks(fontsize=22)
+    plt.yticks(fontsize=22)
+    # plt.title(title, fontsize=22)
+
+    ax = plt.gca()
+    ax.set_xlim([0, xs[-1]])
+    ax.set_ylim([0, max(y_np.flatten())])
+    plt.legend(loc=2, ncol=2, prop={'size': 16})
+    # ax.set_yscale('log')
+    # ax.spines['top'].set_visible(False)
+    # ax.spines['right'].set_visible(False)
+
+    plt.savefig(title + "_average_rank_class_freq.png", dpi=1000)
+
+
+def calculate_divergence(keys, class_freq, base=6):
+    dist = []
+    total_size = len(keys)
+    for i, core_set_percentage in enumerate([0.01, 0.05, 0.1, 0.25, 0.5, 0.75]):
+        core_set_samples = keys[0:int(total_size * core_set_percentage)]
+        # print(core_set_percentage, len(core_set_samples))
+        core_set_class_occ = None
+        # calculate the distribution of classes for the entire core set
+        for cs_sample in core_set_samples:
+            if core_set_class_occ is None:
+                core_set_class_occ = (np.asarray(class_freq[cs_sample]) / (256 * 256))
+            else:
+                core_set_class_occ += (np.asarray(class_freq[cs_sample]) / (256 * 256))
+            # print(core_set_class_occ)
+        core_set_class_prob = softmax(core_set_class_occ / len(core_set_samples))
+        # print(core_set_class_prob, core_set_class_prob.sum())
+
+        # calculate the distance between the previous distribution and
+        # the class distribution of each selected sample
+        dist_total = 0
+        for cs_sample in core_set_samples:
+            class_patch_prob = softmax(np.asarray(class_freq[cs_sample]) / (256 * 256))
+            d = entropy(class_patch_prob, core_set_class_prob, base=base)
+            # print(class_patch_prob, d)
+            dist_total += d
+        dist.append(dist_total / len(core_set_samples))
+    np.set_printoptions(precision=3)
+    print(np.asarray(dist))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='main')
+    parser.add_argument('--operation', type=str, required=True,
+                        choices=['encoded_labels', 'line_plot', 'save_patches', 'multiple_line',
+                                 'acc_class_frequency', 'divergence', 'correlation'])
+    args = parser.parse_args()
+
+    if args.operation == 'encoded_labels':
         # create colored maps for visualization
         dfc22_encode_labels('/mnt/DADOS_PONTOISE_1/keiller/datasets/df2022/labeled_train/Nantes_Saint-Nazaire/')
         dfc22_encode_labels('/mnt/DADOS_PONTOISE_1/keiller/datasets/df2022/labeled_train/Nice/')
-    elif operation in ['line_plot', 'save_patches', 'multiple_line', 'acc_class_frequency']:
+    elif args.operation in ['line_plot', 'save_patches', 'multiple_line', 'acc_class_frequency', 'divergence']:
         feat_based = ['vaihingen_train_coordinate_list_method1.txt', 'ai4gg_diversity.txt']
         label_based = ['vaihingen_train_coordinate_list_method2.txt', 'ai4gg_complexity.txt']
+        # per method
+        ai4gg_complexity = ['ai4gg_complexity.txt', 'ai4gg_complexity.txt', 'ai4gg_complexity.txt']
+        ai4gg_diversity = ['ai4gg_diversity.txt', 'ai4gg_diversity.txt', 'ai4gg_diversity.txt']
+        ai4gg_hybrid = ['ai4gg_hybrid.txt', 'ai4gg_hybrid.txt', 'ai4gg_hybrid.txt']
+        method1 = ['vaihingen_train_coordinate_list_method1.txt', 'potsdam_train_coordinate_list_method1.txt',
+                   'dfc2022_train_coordinate_list_method1.txt']  # feature activation
+        method2 = ['vaihingen_train_coordinate_list_method2.txt', 'potsdam_train_coordinate_list_method2.txt',
+                   'dfc2022_train_coordinate_list_method2.txt']  # class balance
+        hybrid = ['vaihingen_train_coordinate_list_hybrid.txt', 'potsdam_train_coordinate_list_hybrid.txt',
+                  'dfc2022_train_coordinate_list_hybrid.txt']
 
-        vaihingen_kvs = get_vaihingen_kvs(metric='index')
-        potsdam_kvs = get_potsdam_kvs(metric='index')
-        dfc2022_kvs = get_dfc2022_kvs(metric='index')
+        vaihingen_kvs = get_vaihingen_kvs(metric='index', files=ai4gg_hybrid[0])
+        potsdam_kvs = get_potsdam_kvs(metric='index', files=ai4gg_hybrid[1])
+        dfc2022_kvs = get_dfc2022_kvs(metric='index', files=ai4gg_hybrid[2])
         print(len(vaihingen_kvs), len(potsdam_kvs), len(dfc2022_kvs))
 
         vaihingen_keys, vaihingen_mean_vals, vaihingen_std_vals = get_results(vaihingen_kvs)
@@ -560,7 +663,7 @@ if __name__ == "__main__":
 
         # plot_multiple_line_results(vaihingen_xs, vaihingen_ind, vaihingen_mean_vals, vaihingen_std_vals,
         #                            title="Vaihingen", smooth_curve=True, num=20)
-        if operation == 'save_patches':
+        if args.operation == 'save_patches':
             save_patches('/mnt/DADOS_PONTOISE_1/keiller/datasets/vaihingen/Vaihingen/OFFICIAL_DATASET/', vaihingen_keys,
                          'vaihingen', '/mnt/DADOS_PONTOISE_1/keiller/data-centric-rs-classification/patches_label/',
                          patch_size=256, top=10)
@@ -570,13 +673,13 @@ if __name__ == "__main__":
             save_patches('/mnt/DADOS_PONTOISE_1/keiller/datasets/df2022/labeled_train/', dfc2022_keys,
                          'dfc2022', '/mnt/DADOS_PONTOISE_1/keiller/data-centric-rs-classification/patches_label/',
                          patch_size=256, top=10)
-        elif operation == 'multiple_line':
+        elif args.operation == 'multiple_line':
             vaihingen_xs = np.linspace(0, 1, len(vaihingen_keys))
             potsdam_xs = np.linspace(0, 1, len(potsdam_keys))
             dfc2022_xs = np.linspace(0, 1, len(dfc2022_keys))
             plot_multiple_lines(vaihingen_xs, vaihingen_mean_vals, potsdam_xs, potsdam_mean_vals,
                                 dfc2022_xs, dfc2022_mean_vals)
-        elif operation == 'line_plot':
+        elif args.operation == 'line_plot':
             # used in the paper
             vaihingen_xs = np.linspace(1, len(vaihingen_keys), len(vaihingen_keys))
             potsdam_xs = np.linspace(1, len(potsdam_keys), len(potsdam_keys))
@@ -587,22 +690,51 @@ if __name__ == "__main__":
                               "Potsdam", plot_bbs=False, smooth_curve=False, num=0)
             plot_line_results(dfc2022_xs, dfc2022_mean_vals, dfc2022_std_vals,
                               "DFC2022", plot_bbs=False, smooth_curve=False, num=0)
-        elif operation == 'acc_class_frequency':
-            vaihingen_class_freq = get_class_frequency("vaihingen/ai4gg_hybrid.txt",
-                                                       "/mnt/DADOS_PONTOISE_1/keiller/datasets/vaihingen/Vaihingen/"
-                                                       "OFFICIAL_DATASET/gts_for_participants/")
-            print(len(vaihingen_class_freq))
-            with open('vaihingen_class_freq.pkl', 'wb') as f:
-                pickle.dump(vaihingen_class_freq, f)
+        elif args.operation == 'acc_class_frequency':
+            # vaihingen_class_freq = get_class_frequency("vaihingen/ai4gg_hybrid.txt",
+            #                                            "/mnt/DADOS_PONTOISE_1/keiller/datasets/vaihingen/Vaihingen/"
+            #                                            "OFFICIAL_DATASET/gts_for_participants/")
+            # potsdam_class_freq = get_class_frequency("potsdam/ai4gg_hybrid.txt",
+            #                                          "/mnt/DADOS_PONTOISE_1/keiller/datasets/potsdam/"
+            #                                          "Potsdam/5_Labels_all/")
+            # dfc2022_class_freq = get_class_frequency("dfc2022/dfc2022_train_coordinate_list_hybrid.txt",
+            #                                          "/mnt/DADOS_PONTOISE_1/keiller/datasets/df2022/labeled_train/",
+            #                                          dfc=True)
+            # with open('vaihingen_class_freq.pkl', 'wb') as f:
+            #     pickle.dump(vaihingen_class_freq, f)
+            # with open('potsdam_class_freq.pkl', 'wb') as f:
+            #     pickle.dump(potsdam_class_freq, f)
+            # with open('dfc2022_class_freq.pkl', 'wb') as f:
+            #     pickle.dump(dfc2022_class_freq, f)
+            with open('C:\\Users\\keill\\Desktop\\vaihingen_class_freq.pkl', 'rb') as f:
+                vaihingen_class_freq = pickle.load(f)
+            with open('C:\\Users\\keill\\Desktop\\potsdam_class_freq.pkl', 'rb') as f:
+                potsdam_class_freq = pickle.load(f)
+            with open('C:\\Users\\keill\\Desktop\\dfc2022_class_freq.pkl', 'rb') as f:
+                dfc2022_class_freq = pickle.load(f)
 
-            plot_acc_freq(vaihingen_keys, vaihingen_class_freq, "Vaihingen")
-    elif operation == 'correlation':
+            print(len(vaihingen_keys), len(vaihingen_class_freq))
+            print(len(potsdam_keys), len(potsdam_class_freq))
+            print(len(dfc2022_keys), len(dfc2022_class_freq))
+
+            # plot_bar_acc_freq(vaihingen_keys, vaihingen_class_freq, "Vaihingen")
+            plot_line_acc_freq(vaihingen_keys, vaihingen_class_freq, "Vaihingen")
+            plot_line_acc_freq(potsdam_keys, potsdam_class_freq, "Potsdam")
+            plot_line_acc_freq(dfc2022_keys, dfc2022_class_freq, "DFC2022")
+        elif args.operation == 'divergence':
+            with open('C:\\Users\\keill\\Desktop\\vaihingen_class_freq.pkl', 'rb') as f:
+                vaihingen_class_freq = pickle.load(f)
+            with open('C:\\Users\\keill\\Desktop\\potsdam_class_freq.pkl', 'rb') as f:
+                potsdam_class_freq = pickle.load(f)
+            with open('C:\\Users\\keill\\Desktop\\dfc2022_class_freq.pkl', 'rb') as f:
+                dfc2022_class_freq = pickle.load(f)
+
+            calculate_divergence(vaihingen_keys, vaihingen_class_freq, base=6)
+            calculate_divergence(potsdam_keys, potsdam_class_freq, base=6)
+            calculate_divergence(dfc2022_keys, dfc2022_class_freq, base=16)
+    elif args.operation == 'correlation':
         plot_correlation('dfc2022.csv', 'dfc2022')
         plot_correlation('vaihingen.csv', 'vaihingen')
         plot_correlation('potsdam.csv', 'potsdam', True)
-    elif operation == 'divergence':
-        with open('C:\\Users\\keill\\Desktop\\vaihingen_class_freq.pkl', 'rb') as f:
-            vaihingen_class_freq = pickle.load(f)
-
     else:
         raise NotImplementedError
